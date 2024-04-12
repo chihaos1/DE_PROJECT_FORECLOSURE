f16ad961ea8d0a3e40fdb3e7f2360e46ac9ce6c0
unknown <chihaos0629@gmail.com>
2024-04-11 21:22:50 -0400

    Temp update for secret key

--- foreclosure_web/web/settings.py
+++ foreclosure_web/web/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-myelq%!45&69h@cg^&$b9wmg*j0)!s9x+_!+x2kg&%b4*71&_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
